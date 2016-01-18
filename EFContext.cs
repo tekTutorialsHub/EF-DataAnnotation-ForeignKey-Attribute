fac7dae672cd1da5333dff33f9ec44f0dffb16bf
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-18 17:41:33 +0530

    Created

---
+++ EFContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EFForeignKeyAttribute
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("EFDatabase")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+
+
+}
---
+++ models.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFForeignKeyAttribute
+{
+
+
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+
+        public virtual Department Dept { get; set; }
+    }
+
+
+
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
+
+    //public class Employee
+    //{
+    //    public int EmployeeID { get; set; }
+    //    public string Name { get; set; }
+
+    //    public int DepartmentID { get; set; }
+    //    public virtual Department Dept { get; set; }
+    //}
+
+
+
+    //public class Department
+    //{
+    //    public int DepartmentID { get; set; }
+    //    public string Name { get; set; }
+    //}
+
+
+
+        //public class Employee
+        //{
+        //    public int EmployeeID { get; set; }
+        //    public string Name { get; set; }
+
+            
+        //    public int DeptID { get; set; }
+
+        //    [ForeignKey("DeptID")]
+        //    public virtual Department Dept { get; set; }
+        //}
+
+
+
+        //public class Department
+        //{
+        //    public int DepartmentID { get; set; }
+        //    public string Name { get; set; }
+        //}
+
+}
